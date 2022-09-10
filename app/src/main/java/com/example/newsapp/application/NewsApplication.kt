719e68ab6620411ed261d6c99a4756e3d1326764
Hossam Zaabl <hzaabll@gmail.com>
2022-09-10 14:56:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/application/NewsApplication.kt
@@ -0,0 +1,7 @@
+package com.example.newsapp.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NewsApplication : Application()
---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "cd118d4e7c944af68a0a5b4866574d10"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
