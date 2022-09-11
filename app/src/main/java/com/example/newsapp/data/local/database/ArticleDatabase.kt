0c8195b4fe7b94f1a0f6f4ad064c5fd78615761f
Hossam Zaabl <hzaabll@gmail.com>
2022-09-11 17:24:20 +0200

    -Dao & Room Database created "dagger-hilt object injection"

--- app/src/main/java/com/example/newsapp/data/local/database/ArticleDatabase.kt
+++ app/src/main/java/com/example/newsapp/data/local/database/ArticleDatabase.kt
@@ -13,21 +13,4 @@ import com.example.newsapp.data.local.entity.Article
 @TypeConverters(Converters::class)
 abstract class ArticleDatabase : RoomDatabase(){
     abstract fun getArticleDao(): ArticleDao
-
-    companion object{
-        @Volatile
-        private var instance: ArticleDatabase? = null
-        private val LOCK = Any()
-
-        operator fun invoke(context: Context) = instance?: synchronized(LOCK){
-            instance ?: createDatabase(context).also {instance = it}
-        }
-
-        private fun createDatabase(context: Context) =
-            Room.databaseBuilder(
-                context.applicationContext,
-                ArticleDatabase::class.java,
-                "article_db.db"
-            ).build()
-    }
 }
--- app/src/main/java/com/example/newsapp/di/AppModule.kt
+++ app/src/main/java/com/example/newsapp/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.example.newsapp.di
 
+import android.content.Context
+import androidx.room.Room
 import com.example.newsapp.common.Constants
+import com.example.newsapp.data.local.dao.ArticleDao
+import com.example.newsapp.data.local.database.ArticleDatabase
 import com.example.newsapp.data.remote.NewsAPI
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -30,5 +35,20 @@ object AppModule {
             .client(client)
             .build()
             .create(NewsAPI::class.java)
+    }
+
+    @Provides
+    fun provideArticleDao(articleDatabase: ArticleDatabase): ArticleDao {
+        return articleDatabase.getArticleDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext appContext: Context): ArticleDatabase{
+        return Room.databaseBuilder(
+            appContext,
+            ArticleDatabase::class.java,
+            "article_db.db"
+        ).build()
     }
 }
